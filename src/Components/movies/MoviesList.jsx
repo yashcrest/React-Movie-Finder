a79b1f796659683dfbd2b413c1b14ce02aad745b
yashcrest <yashcrest@gmail.com>
2023-08-19 22:30:38 +0930

    styles changes to movielist

--- src/Components/movies/MoviesList.jsx
+++ src/Components/movies/MoviesList.jsx
@@ -8,7 +8,7 @@ const MoviesList = ({popularMovies}) => {
       {popularMovies.map((movie, id) => (
           <div className="col-lg-4 mh-25">
             <div className="card p-3 flex-fill">
-              <h2 className='text-center'>{movie.title || movie.name}</h2>
+              <h3 className='text-center'>{movie.title || movie.name}</h3>
               <img  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className='img-fluid' />
               <p className='mt-4'><span className='bold'>Plot: </span>{movie.overview}</p>
             </div>
