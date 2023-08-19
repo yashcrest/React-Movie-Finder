73f6f9e9003fc4d4e39e5468568ce9b7cb89c7bc
yashcrest <yashcrest@gmail.com>
2023-08-19 10:20:24 +0930

    styling changes

--- src/Components/MovieInfo.jsx
+++ src/Components/MovieInfo.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const MovieInfo = ({movie}) => {
   return (
-    <div className='movie-info text-center'>
+    <div className='movie-info my-5 mx-auto'>
       <h2>{movie.Title} ({movie.Year})</h2>
-      <img className='rounded' src={movie.Poster} alt={movie.Title} />
-      <p>{movie.Plot}</p>
-      <p><span className='lead'>Cast: </span>{movie.Actors}</p>
-      <p>Rating: {movie.imdbRating}</p>
+      <img className='rounded poster' src={movie.Poster} alt={movie.Title} />
+      <p> <span className='synopsis lead'>Synopsis: </span>{movie.Plot}</p>
+      <p><span className='cast lead'>Cast: </span>{movie.Actors}</p>
+      <p><span className='rating lead'>Rating:</span> {movie.imdbRating}</p>
     </div>
   )
 }
