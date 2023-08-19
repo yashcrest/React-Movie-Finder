95f4d88d6878f78f1fbac1f4e2a433aaa0abdbe9
yashcrest <yashcrest@gmail.com>
2023-08-19 13:08:16 +0930

    changed file format

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import React, { useState } from 'react'
-import SearchBar from './Components/SearchBar'
-import SearchResults from './Components/SearchResults';
-import NavBar from './Components/NavBar';
+import Index from './Components/layout/Index';
+import NavBar from './Components/layout/NavBar';
 import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
-import MovieInfo from './Components/MovieInfo';
 
 const App = () => {
-  const [results, setResults] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
-  const [input, setInput] = useState('');
 
-  //handle click on eachresult
-  const handleMovieClick = (movieData) => {
-    setSelectedMovie(movieData)
-    setResults([]); //Clear the search results
-  }
   return (
     <Router>
       <>
         <NavBar />
-        <Routes>
           <div className="container">
-              <SearchBar setResults={setResults} input={input} setInput={setInput}/>
-              <SearchResults results={results} onMovieClick={handleMovieClick} setInput={setInput}/>
-              {selectedMovie && <MovieInfo movie={selectedMovie}/>}
+            <Routes>
+              <Route exact path='/' Component={Index}> </Route>
+            </Routes>
           </div> 
-        </Routes>
       </>
-    </Router>
+    </Router> 
   )
 }
  
