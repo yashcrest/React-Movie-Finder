7a21e0a81365aa18f3bfd8364e4defb225443466
yashcrest <yashcrest@gmail.com>
2023-08-19 10:58:23 +0930

    styles changes done

--- src/Components/MovieInfo.jsx
+++ src/Components/MovieInfo.jsx
@@ -5,9 +5,11 @@ const MovieInfo = ({movie}) => {
     <div className='movie-info my-5 mx-auto'>
       <h2>{movie.Title} ({movie.Year})</h2>
       <img className='rounded poster' src={movie.Poster} alt={movie.Title} />
-      <p> <span className='synopsis lead'>Synopsis: </span>{movie.Plot}</p>
+      <p> <span className='plot lead'>Plot: </span>{movie.Plot}</p>
       <p><span className='cast lead'>Cast: </span>{movie.Actors}</p>
-      <p><span className='rating lead'>Rating:</span> {movie.imdbRating}</p>
+      <p><span className='cast lead'>Director: </span>{movie.Director}</p>
+      <p> <span className='plot lead'>Genre: </span>{movie.Genre}</p>
+      <p><span className='rating lead'>IMDB Rating:</span> {movie.imdbRating} <span>⭐️</span></p>
     </div>
   )
 }
