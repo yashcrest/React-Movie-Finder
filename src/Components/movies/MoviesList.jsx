7ff2eb7637b8bccf5f3ef0e2c80a87dfae70c8d7
yashcrest <yashcrest@gmail.com>
2023-08-19 23:07:10 +0930

    changes to read.me

--- src/Components/movies/MoviesList.jsx
+++ src/Components/movies/MoviesList.jsx
@@ -7,10 +7,12 @@ const MoviesList = ({popularMovies}) => {
       <div className="row d-flex g-5">
       {popularMovies.map((movie, id) => (
           <div className="col-lg-4 mh-25">
-            <div className="card p-3 flex-fill">
+            <div className="card p-3">
               <h3 className='text-center'>{movie.title || movie.name}</h3>
+              <div className="movie-list">
               <img  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className='img-fluid' />
               <p className='mt-4'><span className='bold'>Plot: </span>{movie.overview}</p>
+              </div>
             </div>
           </div>
       ))}
