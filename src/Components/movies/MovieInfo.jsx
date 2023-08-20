2b5f5f625e6974bc8522cf7b3c68ace9834243cf
yashcrest <yashcrest@gmail.com>
2023-08-20 20:52:09 +0930

    adding route

--- src/Components/movies/MovieInfo.jsx
+++ src/Components/movies/MovieInfo.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import {FaStar} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const MovieInfo = ({movie}) => {
   return (
-    <div className='movie-info my-5 mx-auto'>
-      <h2>{movie.Title} ({movie.Year})</h2>
-      <img className='rounded poster' src={movie.Poster} alt={movie.Title} />
-      <p> <span className='plot lead'>Plot: </span>{movie.Plot}</p>
-      <p><span className='cast lead'>Cast: </span>{movie.Actors}</p>
-      <p><span className='cast lead'>Director: </span>{movie.Director}</p>
-      <p> <span className='plot lead'>Genre: </span>{movie.Genre}</p>
-      <p><span className='rating lead'>IMDB Rating:</span> {movie.imdbRating} <FaStar  className='mb-1'/> </p>
-    </div>
+    <>
+    <Link className='link' to='/'>Go back</Link>
+      <div className='movie-info  mx-auto'>
+        <h2>{movie.Title} ({movie.Year})</h2>
+        <img className='rounded poster' src={movie.Poster} alt={movie.Title} />
+        <p> <span className='plot lead'>Plot: </span>{movie.Plot}</p>
+        <p><span className='cast lead'>Cast: </span>{movie.Actors}</p>
+        <p><span className='cast lead'>Director: </span>{movie.Director}</p>
+        <p> <span className='plot lead'>Genre: </span>{movie.Genre}</p>
+        <p><span className='rating lead'>IMDB Rating:</span> {movie.imdbRating} <FaStar  className='mb-1'/> </p>
+      </div>
+    </>
   )
 }
 
 export default MovieInfo
+  
