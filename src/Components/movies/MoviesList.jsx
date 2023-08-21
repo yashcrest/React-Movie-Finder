8d8a060c55ed2e24ad0824866621d04a7431fdc5
yashcrest <yashcrest@gmail.com>
2023-08-21 16:32:25 +0930

    moviesinfo

--- src/Components/movies/MoviesList.jsx
+++ src/Components/movies/MoviesList.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 
 const MoviesList = ({popularMovies}) => {
+
+  //movie clicked state
+  const [clickedmovie, setClickedMovie] = useState('');
+
+  console.log(clickedmovie);
+
   return (
     <div className='movies'>
       <h1 className='text-center m-5 title'>Trending Movies</h1>
       <div className="row d-flex g-5">
       {popularMovies.map((movie, id) => (
-          <div className="col-lg-4 mh-25" key={id}>
+          <div className="col-lg-4 mh-25" key={id} onClick={(e) => setClickedMovie(e)}>
             <div className="card p-3">
               <h3 className='text-center'>{movie.title || movie.name}</h3>
               <div className="movie-list">
