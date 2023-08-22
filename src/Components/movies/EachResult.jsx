ed44307f0b8fc3c17fdb38cf1f8924945b8dd867
yashcrest <yashcrest@gmail.com>
2023-08-22 18:30:08 +0930

    added image on search

--- src/Components/movies/EachResult.jsx
+++ src/Components/movies/EachResult.jsx
@@ -7,9 +7,12 @@ const EachResult = ({result,setInput}) => {
         navigate(`movie/${result.imdbID}`)
         setInput(''); //Clear the input field using React state management
     }
+
+    const posterURL =  `https://image.tmdb.org/t/p/w500/${result.poster_path}`
   return (
       <div className='each-result' onClick={clicked}>
-        {result.Title} ({result.Year})
+        <img src={posterURL} alt={result.title} style={{width:'70px', height:'100px', marginRight:'20px'}} />
+        {result.title}
      </div>
   )
 }
