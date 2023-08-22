bc65dc6494c8747dbaed7ee2be92368feb6f4e37
yashcrest <yashcrest@gmail.com>
2023-08-22 22:09:30 +0930

    decyphering movie info

--- src/Components/movies/MovieInfo.jsx
+++ src/Components/movies/MovieInfo.jsx
@@ -10,7 +10,7 @@ const MovieInfo = ({setResults}) => {
   useEffect(() => {
     const fetchMovieDetails = async () => {
       const tmdb_api_key = import.meta.env.VITE_TMDB_API_KEY;
-      const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${tmdb_api_key}&language=en-US`;
+      const url = `https://api.themoviedb.org/3/movie/${id}?append_to_respond=videos,credits&api_key=${tmdb_api_key}`;
       const response = await fetch(url);
       const movieData = await response.json();
       console.log(movieData);
@@ -28,7 +28,7 @@ const MovieInfo = ({setResults}) => {
         <h2>{movie.original_title} ({movie.release_date})</h2>
         <img className='rounded poster' src={`https://image.tmdb.org/t/p/w400/${movie.poster_path}`} alt={movie.original_title} />
         <p> <span className='plot lead'>Plot: </span>{movie.overview}</p>
-        <p><span className='cast lead'>Cast: </span>{movie.Actors}</p>
+        <p><span className='cast lead'>Cast: </span>{movie.Cast}</p>
         <p><span className='cast lead'>Director: </span>{movie.Director}</p>
         <p> <span className='plot lead'>Genre: </span>{movie.Genre}</p>
         <p><span className='rating lead'>IMDB Rating:</span> {movie.imdbRating} <FaStar  className='mb-1'/> </p>
