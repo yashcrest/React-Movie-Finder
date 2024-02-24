b4bfc9f31e128aee1c96a7a96e7d80cb56fc89c8
Yash Shrestha <yashcrest@gmail.com>
2024-02-24 16:57:41 +1100

    formatting movie info

--- src/Components/movies/MovieInfo.jsx
+++ src/Components/movies/MovieInfo.jsx
@@ -1,11 +1,11 @@
 // this page handles movie detailed view once clicked from homepage
-import React, { useEffect, useState } from 'react'
-import {FaStar} from 'react-icons/fa'
-import {BiArrowBack} from 'react-icons/bi'
-import { Link, useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { FaStar } from "react-icons/fa";
+import { BiArrowBack } from "react-icons/bi";
+import { Link, useParams } from "react-router-dom";
 
-const MovieInfo = ({setResults}) => {
-  const {id} = useParams();
+const MovieInfo = ({ setResults }) => {
+  const { id } = useParams();
   const [movie, setMovie] = useState(null);
 
   useEffect(() => {
@@ -15,45 +15,90 @@ const MovieInfo = ({setResults}) => {
       const creditsUrl = `https://api.themoviedb.org/3/movie/${id}/credits?&api_key=${tmdb_api_key}`;
       const imageUrl = `https://api.themoviedb.org/3/movie/${id}/images?api_key=${tmdb_api_key}`;
 
-      const [detailsResponse , creditsResponse, imageResponse] = await Promise.all([
-        fetch(detailsUrl),
-        fetch(creditsUrl),
-        fetch(imageUrl),
-      ]);
+      const [detailsResponse, creditsResponse, imageResponse] =
+        await Promise.all([
+          fetch(detailsUrl),
+          fetch(creditsUrl),
+          fetch(imageUrl),
+        ]);
 
-      const movieData = await detailsResponse.json()
-      const creditData = await creditsResponse.json()
-      const imageData = await imageResponse.json() //still have to figure out how to handle and display this response
+      const movieData = await detailsResponse.json();
+      console.log("moviedata", movieData);
+      const creditData = await creditsResponse.json();
+      const imageData = await imageResponse.json(); //still have to figure out how to handle and display this response
 
-      const director = creditData.crew.find(person => person.job === 'Director');
-      const cast = creditData.cast.slice(0,5).map(person => person.name).join(', ' ) //getting first 5 casts
+      //filtering result from API before passing to "setMovie"
+      const director = creditData.crew.find(
+        (person) => person.job === "Director"
+      );
+      const cast = creditData.cast
+        .slice(0, 5)
+        .map((person) => person.name)
+        .join(", "); //getting first 5 casts
+
+      //formatting movie date
+      const releaseDate = new Date(movieData.release_date).toLocaleDateString(
+        "en-GB",
+        {
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+        }
+      );
 
       setMovie({
         ...movieData,
-        director: director ? director.name : 'N/A',
+        director: director ? director.name : "N/A",
         cast: cast,
-      })
+        releaseDate: releaseDate,
+      });
     };
 
     fetchMovieDetails();
-  },[id]);
+  }, [id]);
 
-  if(!movie) return <div>Loading...</div>
+  if (!movie) return <div>Loading...</div>;
   return (
     <>
-    <Link className='link h2 text-dark' to='/'><BiArrowBack /></Link>
-      <div className='movie-info  mx-auto mb-5'>
-        <h2>{movie.original_title} ({movie.release_date})</h2>
-        <img className='rounded poster' src={`https://image.tmdb.org/t/p/w400/${movie.poster_path}`} alt={movie.original_title} />
-        <p> <span className='plot lead'>Plot: </span>{movie.overview}</p>
-        <p><span className='cast lead'>Cast: </span>{movie.cast}</p>
-        <p><span className='cast lead'>Director: </span>{movie.director}</p>
-        <p> <span className='plot lead'>Genre: </span>{movie.genres.slice(0,3).map(genre => genre.name).join(', ')}</p>
-        <p><span className='rating lead'>IMDB Rating:</span> {movie.imdbRating} <FaStar  className='mb-1'/> </p>
+      <Link className="link h2 text-dark" to="/">
+        <BiArrowBack />
+      </Link>
+      <div className="movie-info  mx-auto mb-5">
+        <h2 className="movie-name">{movie.original_title}</h2>
+        <p className="movie-date">{movie.releaseDate}</p>
+        <img
+          className="rounded poster"
+          src={`https://image.tmdb.org/t/p/w400/${movie.poster_path}`}
+          alt={movie.original_title}
+        />
+        <p>
+          {" "}
+          <span className="plot lead">Plot: </span>
+          {movie.overview}
+        </p>
+        <p>
+          <span className="cast lead">Cast: </span>
+          {movie.cast}
+        </p>
+        <p>
+          <span className="cast lead">Director: </span>
+          {movie.director}
+        </p>
+        <p>
+          {" "}
+          <span className="plot lead">Genre: </span>
+          {movie.genres
+            .slice(0, 3)
+            .map((genre) => genre.name)
+            .join(", ")}
+        </p>
+        <p>
+          <span className="rating lead">IMDB Rating:</span> {movie.imdbRating}{" "}
+          <FaStar className="mb-1" />{" "}
+        </p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default MovieInfo
-  
+export default MovieInfo;
