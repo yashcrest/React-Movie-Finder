66e224bd864e6375d5433ebaffc5c3f3f1b48441
yash shrestha <yashcrest@gmail.com>
2024-08-27 18:37:28 +1000

    fix: search results are cleared once search result is clicked.

--- src/components/movies/EachResult.tsx
+++ src/components/movies/EachResult.tsx
@@ -2,14 +2,15 @@ import { useNavigate } from "react-router-dom";
 import { useSearchContext } from "../../contexts/SearchContext";
 
 const EachResult = ({ result, onMovieClick }) => {
+  const posterURL = `https://image.tmdb.org/t/p/w500/${result.poster_path}`;
   const { setInput } = useSearchContext();
   const navigate = useNavigate();
   const clicked = async () => {
+    onMovieClick(result);
     navigate(`movie/${result.id}`);
     setInput(""); //Clear the input field using React state management
   };
 
-  const posterURL = `https://image.tmdb.org/t/p/w500/${result.poster_path}`;
   return (
     <div className="each-result" onClick={clicked}>
       <img
